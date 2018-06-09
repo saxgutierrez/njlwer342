2f76df4e642cd7a3af450c985a186b8f41087cbd
saxgutierrez <diegofdog@gmail.com>
2018-06-08 19:14:19 -0500

    solucionando reto 7 react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React, { Component } from 'react';
-
-/// Modifica el componente para que se puedan agregar tareas
-
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {value:"", tasks:["Sacar la ropa","Hacer la cama","Leer un rato"]}
+    this.handleChange = this.handleChange.bind(this);
+    this.keyPress = this.keyPress.bind(this);
+
+  }
+
+   handleChange(event) {
+    this.setState({value: event.target.value});
+    }
+
+   keyPress(event){
+      // if (this.state.value !== ""){
+        if (event.keyCode === 13){
+          event.preventDefault();
+          this.setState({tasks: this.state.tasks.concat(this.state.value)});
+          this.state.value = "";
+        }
+      }
+      // }    
+     
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+            
+            {this.state.tasks.map((task,i)=> <li key={i}>{task}</li>)}
+            
           </ul>
            <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+             <input type="text" id="new-task"  placeholder="Ingresa una tarea y oprime Enter" 
+             value={this.state.value} onKeyDown={this.keyPress}  onChange={this.handleChange} />
            </form>
         </div>
       </div>
     )
   }
 }
-
-
 export default App;
